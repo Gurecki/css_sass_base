823e619a1ec39a85c70679762a3c710ac56bc1ac
Marcin Pietrzyk <contact@marcinpietrzyk.pl>
2022-12-19 14:49:10 +0100

    Random and stagger

--- src/scripts/components/boxes.ts
+++ src/scripts/components/boxes.ts
@@ -2,5 +2,5 @@ import gsap from "gsap"
 
 
 (function animateBoxes() {
-    gsap.to('.boxes__box:first-child', {duration: 10, translateX: 100,backgroundColor: 'pink', repeat: -1, yoyo: true, ease: 'bounce'})
+    gsap.to('.boxes__box', {duration: 0.5, translateX: 'random(200, 600)',backgroundColor: 'pink', repeat: -1, yoyo: true, ease: 'ease', stagger: 0.1})
 })()
