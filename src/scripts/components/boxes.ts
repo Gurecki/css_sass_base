5193848fc355473b5f1166746ce1813af86c7cd2
Marcin Pietrzyk <contact@marcinpietrzyk.pl>
2022-12-19 15:26:57 +0100

    Scroll trigger

--- src/scripts/components/boxes.ts
+++ src/scripts/components/boxes.ts
@@ -1,6 +1,23 @@
 import gsap from "gsap"
-
-
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 (function animateBoxes() {
-    gsap.to('.boxes__box', {duration: 0.5, translateX: 'random(200, 600)',backgroundColor: 'pink', repeat: -1, yoyo: true, ease: 'ease', stagger: 0.1})
+
+    gsap.registerPlugin(ScrollTrigger);
+    // const tween = gsap.to('.boxes__box', {duration: 0.5, translateX: 'random(200, 600)',backgroundColor: 'pink', repeat: -1, yoyo: true, ease: 'ease', stagger: 0.1})
+
+    const tl = gsap.timeline(
+        { 
+            scrollTrigger: {
+                trigger: '.boxes__box:first-child',
+                start: 'top center',
+                markers: true,
+                scrub: true,
+                toggleClass: 'my-class'
+            },
+            yoyo: true,
+        }
+        );
+    tl.to('.boxes__box', {duration: 1, translateX: 'random(200, 600)', ease: 'ease', 
+});
+    // tl.to('.boxes__box:first-child', {backgroundColor: 'blue'});
 })()
